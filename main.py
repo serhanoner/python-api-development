f301d89281fd4dfe12909ac9b4621f3ed425a085
Serhan Oner Aksakal <aksakalserhan@gmail.com>
2023-11-06 17:50:07 +0300

    get request with a dynamic id function has added.

--- main.py
+++ main.py
@@ -15,10 +15,16 @@ class Post(BaseModel):
 
 my_posts = [ {"title": "title of post 1",
               "content": "content of post 1",
-              "id": 2},
+              "id": 1},
              {"title": "favorite foods",
               "content": "I like pizza",
               "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 
 @app.get("/")
 async def root():
@@ -36,3 +42,8 @@ def create_posts(post: Post):
     return {"data": post_dict}
 # title str, content str
 
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post = find_post(id)
+    print(post)
+    return {"post_detail": post}
