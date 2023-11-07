40d5e11ef843ca54bf8d5b89e88493a5cf8e2ca9
Serhan Oner Aksakal <aksakalserhan@gmail.com>
2023-11-07 09:56:28 +0300

    declared the http status code on the decorator of the post request.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import fastapi
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, exceptions
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
@@ -30,11 +30,13 @@ def find_post(id):
 async def root():
     return {"message": "Hello World"}
 
+
+
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts",  status_code = status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,10000)
@@ -42,8 +44,23 @@ def create_posts(post: Post):
     return {"data": post_dict}
 # title str, content str
 
+
+# THE ORDER IS CRITICAL
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return {"detail": post}
+
+# THE ORDER IS CRITICAL
 @app.get("/posts/{id}")
-def get_post(id:int):
+def get_post(id:int,  response: Response):
+
     post = find_post(id)
-    print(post)
+    if not post:
+        raise exceptions.HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                       detail=f"post with id: {id} not found")
+    #    response.status_code = status.HTTP_404_NOT_FOUND
+    #   return  {'message': f"post with id: {id} not found"}
+
     return {"post_detail": post}
+
